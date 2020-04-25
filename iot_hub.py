f0ef55030a38378926938a5627a424783ef3d07d
Prasenjit Chatterjee <Prasenjit2564@gmail.com>
2020-04-25 17:21:10 +0530

    iot hub and therm files updated

--- iot_hub.py
+++ iot_hub.py
@@ -8,40 +8,35 @@ class iot_hub:
     # The device connection string to authenticate the device with your IoT hub.
     # Using the Azure CLI:
     # az iot hub device-identity show-connection-string --hub-name {YourIoTHubName} --device-id MyNodeDevice --output table
-    CONNECTION_STRING = "HostName=azurecrusaders.azure-devices.net;DeviceId=CaliberDeviceId;SharedAccessKey=kT+ZzVvq76QHe4wWSKnUCEDr2jr7OziPSQ4G0hEa+t8="
+    #CONNECTION_STRING = "HostName=azurecrusaders.azure-devices.net;DeviceId=CaliberDeviceId;SharedAccessKey=kT+ZzVvq76QHe4wWSKnUCEDr2jr7OziPSQ4G0hEa+t8="
     RECEIVED_MESSAGES = 0
+    client= None
 
-    def iothub_client_init(self):
+    def __init__(self, connection_string):
         # Create an IoT Hub client
-        client = IoTHubDeviceClient.create_from_connection_string(iot_hub.CONNECTION_STRING)
-        return client
+        self.client = IoTHubDeviceClient.create_from_connection_string(iot_hub.CONNECTION_STRING)        
 
-    def iothub_send_message(self, message):
+    def send_message(self, message):
         try:
-            client = iot_hub.iothub_client_init(self)            
-            # Send the message.
-            print( "Sending message: {}".format(message) )
-            client.send_message(message)
-            print ( "Message successfully sent" )
-            #time.sleep(3)
+            self.client.send_message(message)
+            print( " Sent message: {}".format(message) )
 
         except KeyboardInterrupt:
             print ( "IoTHubClient stopped" )
 
-    def message_listener(self, client):
+    def message_listener(self):
         #global RECEIVED_MESSAGES
         while True:
-            message = client.receive_message()
+            message = self.client.receive_message()
             iot_hub.RECEIVED_MESSAGES += 1
             print("Message received")
             print( "    Data: {}".format(message.data) )
             print( "    Properties: {}".format(message.custom_properties))
             print( "    Total calls received: {}".format(iot_hub.RECEIVED_MESSAGES))
 
-    def iothub_receive_message(self):
-        try:
-            client = IoTHubDeviceClient.create_from_connection_string(iot_hub.CONNECTION_STRING)
-            message_listener_thread = threading.Thread(target=iot_hub.message_listener, args=(self,client,))
+    def receive_message(self):
+        try:            
+            message_listener_thread = threading.Thread(target=iot_hub.message_listener, args=(self,))
             message_listener_thread.daemon = True
             message_listener_thread.start()
 
--- therm_sensor.py
+++ therm_sensor.py
@@ -1,11 +1,27 @@
+import iot_hub
+import dht
+
+from dht import *
+from iot_hub import *
 from w1thermsensor import W1ThermSensor
  
-body_temp_sensor = W1ThermSensor()
 
 class therm_sensor:
-    bodyTempSensor = W1ThermSensor()
+    body_temp_sensor = None
     temperature = 4.0
+    iot_hub= None
+    dht = None
+    hub_message = '{{"roomtemperature": {temperature},"humidity": {humidity}, "bodytemperature": {bodytemperature}}}'
 
-    def __init__(self):        
-        self.temperature = body_temp_sensor.get_temperature()
+    def __init__(self, connection_string):
+        self.iot_hub=iot_hub(connection_string)
+        self.dht = dht(26)
+        self.body_temp_sensor = W1ThermSensor()
+        #self.temperature = body_temp_sensor.get_temperature()
     
+    def post_to_iot_hub(self):
+        dhtout = self.dht.get_temperature()
+        msg_txt_formatted = self.hub_message.format(roomtemperature=dhtout.temperature, humidity=dhtout.humidity, bodytemperature=self.body_temp_sensor.get_temperature())
+        self.iot_hub.send_message(msg_txt_formatted)
+
+        
